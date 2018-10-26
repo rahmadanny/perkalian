10a128cef3d4885c336ba84cd3bc6161ad1e2831
hmmmm <44452028+rahmadanny@users.noreply.github.com>
2018-10-26 11:47:50 +0000

    Add files via upload

---
+++ genap atau ganjil.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ main ()
+ { int angka;
+	printf("masukan bilangan=");
+	scanf("%d",&angka);
+   if (angka%2==0)
+   	
+   {printf("angka %d" ,angka );
+   printf(" adalah genap");
+   	
+   }else {printf("angka %d" ,angka );
+   		  printf(" adalah ganjil");
+   }
+ }
